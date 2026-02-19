cadd0739de347f961be1cad24df9953b37431166
athmika2904 <athmika.cs24@bmsce.ac.in>
2026-02-19 19:07:59 +0530

    css added

--- project1/src/Component/History.jsx
+++ project1/src/Component/History.jsx
@@ -1,10 +1,10 @@
 function History({history}){
     return(
-        <div>
+        <div className="chat-container">
             {history.map((item,index)=>(
                 <div key={index}>
-                    <p>You:{item.user}</p>
-                    <p>Voice:{item.reply}</p>
+                    <div className="user">You:{item.user}</div>
+                    <div className="voice">Voice:{item.reply}</div>
                 </div>
             ))}
         </div>
